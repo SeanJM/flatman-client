62a072bd0e9fe47337ce2436ea2b9b45755d297c
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-06-09 17:29:46 -0400

    Can now accept multiple types of structures to create a node.

--- src/prototype/createNode.js
+++ src/prototype/createNode.js
@@ -1,5 +1,31 @@
-function CreateNode (target, opt, text) {
+/*
+  Argument format
+  CreateNode([String], [Object], [Text | CreateNode Object | Array | Node ])
+*/
+
+function CreateNode () {
   var self = this;
+  var target;
+  var opt;
+  var child;
+
+  var i = 0;
+  var n = arguments.length;
+  var a = new Array(n);
+
+  for (; i < n; i++) {
+    a[i] = arguments[i];
+  }
+
+  if (isArray(a[0]) && a.length === 1) {
+    target = a[0][0];
+    opt = a[0][1];
+    child = a[0].length <= 3 ? a[0][2] : a[0].slice(2);
+  } else {
+    target = a[0];
+    opt = a[1];
+    child = a.length <= 3 ? a[2] : a.slice(2);
+  }
 
   this._class_ = [];
 
@@ -17,12 +43,16 @@ function CreateNode (target, opt, text) {
   setNode(this, target);
   setAttributes(this._node_, opt);
   normalizeTextInput(this);
-
-  if (typeof text === 'string') {
-    this._node_.innerHTML = text;
-  }
+  appendChild(this._node_, child);
 }
 
-function createNode (target, opt, text) {
-  return new CreateNode(target, opt, text);
+function createNode (target, opt, child) {
+  switch (arguments.length) {
+    case 1 :
+    return new CreateNode(target);
+    case 2 :
+    return new CreateNode(target, opt);
+    default :
+    return new CreateNode(target, opt, child);
+  }
 }
