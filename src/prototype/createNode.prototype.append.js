7aabc3f169b88bad1e3bb60463b02d2379f7fbd4
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-06-10 17:09:34 -0400

    Removed extra step in append

--- src/prototype/createNode.prototype.append.js
+++ src/prototype/createNode.prototype.append.js
@@ -1,23 +1,8 @@
 CreateNode.prototype.append = function () {
   var n = arguments.length;
-  var a = new Array(n);
   var i = 0;
-  var child;
-
   for (; i < n; i++) {
-    a[i] = arguments[i];
+    appendChild(this.node, arguments[i]);
   }
-
-  // Is a 'CreateNode' element
-  if (a[0] instanceof CreateNode) {
-    i = 0;
-    for (; i < n; i++) {
-      this.node.appendChild(a[i].node);
-    }
-  } else {
-    child = createNode.apply(null, a);
-    this.node.appendChild(child.node);
-  }
-
   return this;
 };
