4c29ca53aecbe90eac4caa0d492eb810ace44ece
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-06-10 12:38:45 -0400

    Offloaded to a shared function 'removeClass'

--- src/prototype/createNode.prototype.removeClass.js
+++ src/prototype/createNode.prototype.removeClass.js
@@ -1,6 +1,4 @@
 CreateNode.prototype.removeClass = function (a) {
-  this.node.className = filter(map(this.node.className.split(' '), trim), function (b) {
-    return hasLength(b) && not(a, b);
-  }).sort().join(' ');
+  removeClass(this.node, a);
   return this;
 };
