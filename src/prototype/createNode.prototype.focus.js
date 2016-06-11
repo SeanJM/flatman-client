4efda39d4706db7ca07b012ed75fc34ee131e757
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-06-11 11:59:31 -0400

    'focus' checks for the 'tabIndex' attribute and adds it if it does not exist.

--- src/prototype/createNode.prototype.focus.js
+++ src/prototype/createNode.prototype.focus.js
@@ -1,4 +1,9 @@
 CreateNode.prototype.focus = function () {
+  if (!this.node.getAttribute('tabindex')) {
+    this.node.setAttribute('tabindex', '0');
+  }
+
   this.node.focus();
+
   return this;
 };
