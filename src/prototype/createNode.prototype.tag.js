0fb98c852a607dcab0956e12cf6e8f14dfe69ec7
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-06-10 20:38:05 -0400

    Fixed missing reassignment of 'this.node' to 'clone.node'

--- src/prototype/createNode.prototype.tag.js
+++ src/prototype/createNode.prototype.tag.js
@@ -2,13 +2,14 @@ CreateNode.prototype.tag = function (name) {
   var clone;
 
   if (typeof name === 'undefined') {
-    return this.node.tagName;
+    return this.node.tagName.toLowerCase();
   }
 
   clone = new CreateNode(name);
   clone.text(this.node.innerHTML);
   clone.copyAttributes(this.node);
   this.replaceWith(clone);
+  this.node = clone.node;
 
   return clone;
 };
