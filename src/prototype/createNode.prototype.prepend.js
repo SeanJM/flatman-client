6f0cc2570ad52bcc9ed6c7d968da877d8e9fc216
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-06-10 20:14:55 -0400

    children will return 'false' if it has none, so removed check for '.length'

--- src/prototype/createNode.prototype.prepend.js
+++ src/prototype/createNode.prototype.prepend.js
@@ -1,6 +1,6 @@
 CreateNode.prototype.prepend = function (node) {
   var children = this.children();
-  if (children.length) {
+  if (children) {
     createNode(node).before(children[0]);
   } else {
     createNode(node).appendTo(this);
