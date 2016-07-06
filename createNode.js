a6fd04a568b636e280dbc40de03f15d6260b6608
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-07-06 11:51:45 -0400

    rewrote isVisible

--- createNode.js
+++ createNode.js
@@ -357,42 +357,52 @@
   
 
   function isVisible() {
+    var windowWidth = window.innerWidth;
+    var windowTop = window.pageYOffset;
+    var html = document.getElementsByTagName('html')[0];
+  
     function is(node) {
       var css = window.getComputedStyle(node);
       var rect = node.getBoundingClientRect();
       return ((
-        rect.left + rect.width > 0 &&
-        rect.left < window.innerWidth &&
-        rect.top + rect.height + window.pageYOffset > 0
+        rect.left + rect.width > 0
+          && rect.left < windowWidth
+          && rect.top + rect.height + windowTop > 0
       ) && (
-        rect.width > 0 &&
-        rect.height > 0
+        rect.width > 0
+          && rect.height > 0
       ) && (
-        css.overflow !== 'hidden' &&
-        css.visibility !== 'none' &&
-        css.display !== 'none'
+        css.visibility !== 'none'
+          && css.display !== 'none'
       ));
     }
   
-    function check(element) {
-      var parents = element.parents();
-      var n = parents.length;
-      var i = 0;
+    function isDeep(element) {
+      var n;
+      var i;
+      var parent;
+  
+      var node = element instanceof CreateNode
+        ? element.node
+        : element;
   
       // Check parents for visibility
-      if (is(element.node)) {
-        for (; i < n; i++) {
-          if (!is(parents[i].node)) {
+      if (!is(node)) {
+        return false;
+      } else {
+        parent = node.parentNode;
+        while (parent && parent !== html) {
+          if (!is(parent)) {
             return false;
           }
+          parent = parent.parentNode;
         }
-        return true;
       }
-      return false;
+      return true;
     }
   
     for (var i = 0, n = arguments.length; i < n; i++) {
-      if (!is(arguments[i])) {
+      if (!isDeep(arguments[i])) {
         return false;
       }
     }
