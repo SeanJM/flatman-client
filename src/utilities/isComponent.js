ed1a7935c75cc25c70dd4b64b2bc39a89e3df2cd
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-07-16 17:25:19 -0400

    isComponent function

---
+++ src/utilities/isComponent.js
@@ -0,0 +1,7 @@
+function isComponent(C) {
+  return (
+    typeof C === 'object'
+    && C.constructor.name !== 'Object'
+    && C.constructor.name[0][0].toUpperCase() === C.constructor.name[0][0]
+  );
+}
