0bd5d7b7f0fabf0593a3da566822e4a3cc72e9e5
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-07-16 17:25:57 -0400

    Added whitespace

--- src/prototype/createNode.prototype.append.js
+++ src/prototype/createNode.prototype.append.js
@@ -1,8 +1,10 @@
 CreateNode.prototype.append = function () {
   var n = arguments.length;
   var i = 0;
+
   for (; i < n; i++) {
     appendChild(this.node, arguments[i]);
   }
+
   return this;
 };
