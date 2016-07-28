68fa04fbd3497c0a2a405e26ac58f6ccc0e7ee8b
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-07-28 17:45:25 -0400

    Better component detection

--- src/utilities/isComponent.js
+++ src/utilities/isComponent.js
@@ -1,8 +1,8 @@
 function isComponent(C) {
   return (
     typeof C === 'object'
+    && Object.prototype.toString.call(C) === '[object Object]'
     && C.constructor.name !== 'Object'
-    && C.constructor.name !== 'Array'
     && C.constructor.name[0][0].toUpperCase() === C.constructor.name[0][0]
   );
 }
