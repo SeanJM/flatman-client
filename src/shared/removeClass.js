1aa6e3b7c47c9fe152b4373ac694230b33d8a2f8
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-30 18:36:01 -0400

    added support for arrays

--- src/shared/removeClass.js
+++ src/shared/removeClass.js
@@ -1,5 +1,9 @@
 function removeClass (node, a) {
-  node.className = filter(map(node.className.split(' '), trim), function (b) {
-    return hasLength(b) && not(a, b);
-  }).sort().join(' ');
+  if (isArray(a)) {
+    forEach(a, partial(removeClass, node));
+  } else {
+    node.className = filter(map(node.className.split(' '), trim), function (b) {
+      return hasLength(b) && not(a, b);
+    }).sort().join(' ');
+  }
 }
