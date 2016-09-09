a187c0f2fcc96b53787c6ede6912621f89110717
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-09-09 07:52:45 -0400

    Fixing links pt2

--- grunt/readme/generate.js
+++ grunt/readme/generate.js
@@ -24,13 +24,13 @@ function tableOfContents(text, content, i) {
       value.forEach(function (a) {
         let name = smartCase(path.basename(a).replace(/\.md$/, ''));
         var base = a.slice(source.length, -3);
-        text.push(new Array(i + 1).join('  ') + '- [' + name + '](#' + _.kebabCase(base).split('-').join('--') + ')');
+        text.push(new Array(i + 1).join('  ') + '- [' + name + '](#' + _.kebabCase(base).split('-').join('--') + '-top)');
       });
     } else if (typeof value === 'object') {
       tableOfContents(text, value, i + 1);
     } else if (typeof value === 'string') {
       var base = value.substr(source.length);
-      text.push(new Array(i).join('  ') + '- [' + smartCase(key) + '](#' + _.kebabCase(base).split('-').join('--') + ')');
+      text.push(new Array(i).join('  ') + '- [' + smartCase(key) + '](#' + _.kebabCase(base).split('-').join('--') + '-top)');
     }
   });
 }
