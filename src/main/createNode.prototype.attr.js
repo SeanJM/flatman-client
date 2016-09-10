a41a9fb09e2cfa27c82f26fbca27fbc1dd93174a
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-09-10 17:07:47 -0400

    replaced long form conversion of attributes to an object with 'getAttributes'

--- src/main/createNode.prototype.attr.js
+++ src/main/createNode.prototype.attr.js
@@ -1,21 +1,15 @@
 CreateNode.prototype.attr = function () {
-  var attr;
-  var res;
-  if (typeof arguments[0] === 'string' && typeof arguments[1] === 'string') {
+  if (arguments.length === 0) {
+    return getAttributes(this.node);
+  } else if (
+    typeof arguments[0] === 'string'
+    && typeof arguments[1] === 'string'
+  ) {
     this.node.setAttribute(arguments[0], arguments[1]);
   } else if (typeof arguments[0] === 'string') {
     return this.node.getAttribute(arguments[0]);
   } else if (typeof arguments[0] === 'object') {
     setAttributes(this.node, arguments[0]);
-  } else if (!arguments.length) {
-    attr = this.node.attributes;
-    res = {};
-
-    for (var i = 0, n = attr.length; i < n; i++) {
-      res[attr[i].nodeName] = attr[i].nodeValue;
-    }
-
-    return res;
   }
 
   return this;
