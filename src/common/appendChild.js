7b2f9b669d99eab3e29df131dc809ac61543256f
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-12-06 11:34:15 -0500

    Added a guard to ensure that there is a 'children' array

--- src/common/appendChild.js
+++ src/common/appendChild.js
@@ -1,11 +1,13 @@
 function appendChild(node, children) {
   var f = document.createDocumentFragment();
-  node = getNode(node);
+  if (children.length) {
+    node = getNode(node);
 
-  for (var i = 0, n = children.length; i < n; i++) {
-    f.appendChild(getNode(children[i]));
+    for (var i = 0, n = children.length; i < n; i++) {
+      f.appendChild(getNode(children[i]));
+    }
+
+    node.appendChild(f);
+    mount(node);
   }
-
-  node.appendChild(f);
-  mount(node);
 }
