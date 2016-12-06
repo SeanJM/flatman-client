504229ccc347aa284f1072b6de646b31a929e8c1
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-12-06 11:36:20 -0500

    Added 'SVGU' for a valid 'isElement' return value of true

--- src/predicates/isElement.js
+++ src/predicates/isElement.js
@@ -1,5 +1,5 @@
 function isElement (a) {
-  var validElements = ['HTML', 'SVGS'];
+  var validElements = ['HTML', 'SVGS', 'SVGU'];
   var stringValue = Object.prototype.toString.call(a).substr(8, 4);
   return validElements.indexOf(stringValue) !== -1;
 }
