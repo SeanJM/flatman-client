6d12eeefb0747f295f9a54b4d509ae3e91229435
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-06 09:02:55 -0500

    fixed 'trigger' to work with event object

--- src/main/Node/Node.prototype.trigger.js
+++ src/main/Node/Node.prototype.trigger.js
@@ -1,40 +1,43 @@
 Node.prototype.trigger = function () {
-  var names = arguments[0];
-  var e = arguments[1];
+  var subscribers = this.subscribers;
+  var names;
+  var eventOpt;
+  var callbacks;
 
-  if (typeof arguments[0] === 'object') {
+  function preventDefault() {
+    eventOpt.defaultPrevented = true;
+  }
+
+  if (typeof arguments[0] === 'string') {
+    names = arguments[0];
+    eventOpt = arguments[1] || {};
+  } else {
     names = arguments[0].type;
-    e = arguments[0];
+    eventOpt = arguments[0];
   }
 
-  function trigger(self, callback) {
-    callback.call(self, e);
+  if (typeof eventOpt.preventDefault === 'undefined') {
+    eventOpt.defaultPrevented = false;
+    eventOpt.preventDefault = preventDefault;
   }
 
-  if (e && typeof e.preventDefault === 'undefined') {
-    e.defaultPrevented = false;
-    e.preventDefault = function () {
-      e.defaultPrevented = true;
-    };
-  }
-
+  eventOpt.type = eventOpt.type || names;
+  eventOpt.target = eventOpt.target || this.node;
   names = names.toLowerCase().split(',');
 
-  if (typeof e === 'undefined') {
-    e = { type : name, target : this.node };
-  } else if (typeof e.type === 'undefined') {
-    e.type = name;
-  }
+  if (!this.node.disabled && subscribers) {
+    names.forEach(function (name) {
+      name = name.trim();
 
-  if (!this.node.disabled) {
-    for (var i = 0, n = names.length; i < n; i++) {
-      names[i] = names[i].trim();
-      if (names[i].length && this.subscribers[names[i]]) {
-        for (var x = this.subscribers[names[i]].length - 1; x >= 0; x--) {
-          trigger(this, this.subscribers[names[i]][x]);
-        }
+      if (subscribers[name]) {
+        subscribers[name]
+          .slice()
+          .forEach(function (callback) {
+            callback.call(self, eventOpt);
+          });
       }
-    }
+
+    });
   }
 
   return this;
