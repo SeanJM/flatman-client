2f2c1c2278c08e9f73f17989db2753f22436af89
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-06 09:03:14 -0500

    remove node

--- src/common/remove.js
+++
@@ -1,4 +0,0 @@
-function remove(element) {
-  getNode(element.parentNode).removeChild(element.node);
-  unmount(element);
-}
--- src/main/Node/Node.prototype.remove.js
+++ src/main/Node/Node.prototype.remove.js
@@ -1,4 +1,19 @@
 Node.prototype.remove = function () {
-  remove(this);
+  var onBody = this.hasParent(BODY);
+  var index = -1;
+
+  if (this.parentNode) {
+    index = this.parentNode.childNodes.indexOf(this);
+    this.parentNode.childNodes.splice(index, 1);
+    getNode(this.parentNode).removeChild(getNode(this));
+    this.parentNode = undefined;
+    this.trigger('remove');
+  }
+
+  if (onBody) {
+    unmount(this);
+  }
+
+
   return this;
 };
