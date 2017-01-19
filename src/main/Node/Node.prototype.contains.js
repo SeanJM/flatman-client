edd77d378f3d02df6929fdeb9ddef5a39085e367
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-18 22:12:12 -0500

    renamed 'each' to 'contains'

--- src/main/Node/Node.prototype.contains.js
+++ src/main/Node/Node.prototype.contains.js
@@ -1,19 +1,16 @@
 Node.prototype.contains = function (maybeList) {
-  var i = 1;
-  var n = arguments.length;
-
-  function each(node, a) {
+  function contains(node, a) {
     a = getNode(a);
     return node.contains(a) && a !== node;
   }
 
   if (Array.isArray(maybeList)) {
     for (var x = 0, y = maybeList.length; x < y; x++) {
-      if (each(this.node, maybeList[x])) {
+      if (contains(this.node, maybeList[x])) {
         return true;
       }
     }
   }
 
-  return each(this.node, maybeList);
+  return contains(this.node, maybeList);
 };
