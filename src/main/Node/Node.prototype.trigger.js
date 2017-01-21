72945d65e6d57eb852be2558d9187f718c8dfe1c
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-21 09:12:40 -0500

    made function smaller

--- src/main/Node/Node.prototype.trigger.js
+++ src/main/Node/Node.prototype.trigger.js
@@ -1,42 +1,26 @@
 Node.prototype.trigger = function () {
   var subscribers = this.subscribers;
   var names;
-  var eventOpt;
-  var callbacks;
-
-  function preventDefault() {
-    eventOpt.defaultPrevented = true;
-  }
+  var detail;
 
   if (typeof arguments[0] === 'string') {
     names = arguments[0];
-    eventOpt = arguments[1] || {};
+    detail = arguments[1] || {};
   } else {
     names = arguments[0].type;
-    eventOpt = arguments[0];
+    detail = arguments[0];
   }
 
-  if (typeof eventOpt.preventDefault === 'undefined') {
-    eventOpt.defaultPrevented = false;
-    eventOpt.preventDefault = preventDefault;
-  }
-
-  eventOpt.type = eventOpt.type || names;
-  eventOpt.target = eventOpt.target || this.node;
   names = names.toLowerCase().split(',');
 
-  if (!this.node.disabled && subscribers) {
+  if (!this.node.disabled) {
     names.forEach(function (name) {
       name = name.trim();
-
-      if (subscribers[name]) {
-        subscribers[name]
-          .slice()
-          .forEach(function (callback) {
-            callback.call(self, eventOpt);
-          });
+      if (name.length && subscribers[name]) {
+        subscribers[name].forEach(function (callback) {
+          callback.call(self, Object.assign({}, detail, { type : name }));
+        });
       }
-
     });
   }
 
