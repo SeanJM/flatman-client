165aebdbb1822b0a49f7e0e71d6d9185b14748f7
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-21 09:10:40 -0500

    made function smaller

--- src/main/Node/Node.prototype.off.js
+++ src/main/Node/Node.prototype.off.js
@@ -1,24 +1,20 @@
 Node.prototype.off = function (names, callback) {
-  names = names.toLowerCase().split(',');
+  var self = this;
 
-  for (var i = 0, n = names.length; i < n; i++) {
-    names[i] = names[i].trim();
-
-    if (names[i].length) {
-      if (typeof callback === 'function') {
-        this.node.removeEventListener(names[i], callback, false);
-        this.subscribers[names[i]] = this.subscribers[names[i]]
-          .filter(function ($callback) {
-            return $callback !== callback;
-          });
-      } else {
-        while (this.subscribers[names[i]].length) {
-          this.node.removeEventListener(names[i], this.subscribers[names[i]][0], false);
-          this.subscribers[names[i]].shift();
-        }
-      }
-    }
+  function each(name, callback) {
+    var subscribers = self.subscribers[name];
+    self.node.removeEventListener(name, callback);
+    subscribers.splice(subscribers.indexOf(callback), 1);
   }
 
+  names = names.toLowerCase().split(',');
+  for (var i = 0, n = names.length; i < n; i++) {
+    names[i] = names[i].trim();
+    if (names[i].length && callback) {
+      each(names[i], callback);
+    } else while (this.subscribers[names[i]].length) {
+      each(names[i], this.subscribers[names[i]][0]);
+    }
+  }
   return this;
 };
