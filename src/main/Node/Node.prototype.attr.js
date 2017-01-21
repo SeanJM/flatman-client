29d131820708e5e163eb11c6aaf160fddc8f6417
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-21 09:08:08 -0500

    cleaner handling of attributes object

--- src/main/Node/Node.prototype.attr.js
+++ src/main/Node/Node.prototype.attr.js
@@ -1,38 +1,52 @@
 Node.prototype.attr = function () {
-  var attr = this.node.attributes;
   var attrObject = {};
+  var attr = this.node.attributes;
   var self = this;
+
+  function setAttrPropValue(prop, value) {
+    if (self.isSVG) {
+      self.node.setAttributeNS('http://www.w3.org/1999/xlink', prop, value);
+    } else if (prop === 'className') {
+      self.className(value);
+    } else if (prop === 'style') {
+      self.style(value);
+    } else if ('once' === prop.substr(0, 4)) {
+      self.once(prop.substr(4), value);
+    } else if ('on' === prop.substr(0, 2)) {
+      self.on(prop.substr(2), value);
+    } else {
+      prop === 'tabindex'
+        ? 'tabIndex'
+        : prop;
+      self.node.setAttribute(prop, value);
+    }
+    return self;
+  }
+
+  function setAttr(prop, value) {
+    if (typeof prop === 'string' && typeof value !== 'undefined') {
+      return setAttrPropValue(prop, value);
+    } else if (typeof prop === 'string') {
+      return self.node.getAttribute(prop);
+    }
+  }
+
+  function setAttrObject(attr) {
+    for (var k in attr) {
+      setAttr(k, attr[k]);
+    }
+  }
 
   if (arguments.length === 0) {
     for (var i = 0, n = attr.length; i < n; i++) {
       attrObject[attr[i].nodeName] = attr[i].nodeValue;
     }
     return attrObject;
-  } else if (
-    typeof arguments[0] === 'string'
-    && typeof arguments[1] === 'string'
-  ) {
-    if (this.isSVG) {
-      this.node.setAttributeNS('http://www.w3.org/1999/xlink', arguments[0], arguments[1]);
-    } else {
-      this.node.setAttribute(arguments[0], arguments[1]);
-    }
-  } else if (typeof arguments[0] === 'string') {
-    return this.node.getAttribute(arguments[0]);
-  } else if (typeof arguments[0] === 'object') {
-    for (var k in arguments[0]) {
-      if (k === 'className') {
-        this.className(arguments[0][k]);
-      } else if (k === 'style') {
-        this.style(arguments[0][k]);
-      } else if ('once' === k.substr(0, 4)) {
-        this.once(k.substr(4), arguments[0][k]);
-      } else if ('on' === k.substr(0, 2)) {
-        this.on(k.substr(2), arguments[0][k]);
-      } else {
-        this.node.setAttribute(k === 'tabindex' ? 'tabIndex' : k, arguments[0][k]);
-      }
-    }
   }
-  return this;
+
+  if (typeof arguments[0] === 'object') {
+    return setAttrObject(arguments[0]);
+  }
+
+  return setAttr(arguments[0], arguments[1]);
 };
