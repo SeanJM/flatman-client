3acc04ff3a7860c45ffd4dfda6141cb526d97ed7
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-21 09:09:30 -0500

    find will support older browsers and predicates

--- src/main/Node/Node.prototype.find.js
+++ src/main/Node/Node.prototype.find.js
@@ -1,5 +1,48 @@
-Node.prototype.find = function (selector) {
-  return [].map.call(this.node.querySelectorAll(selector), function (node) {
-    return el(node);
-  });
-};
+(function () {
+  function findPredicate(predicate) {
+    var found = [];
+
+    function find(childNodes) {
+      childNodes.forEach(function (element) {
+        if (element.children) {
+          if (predicate(element)) {
+            found.push(element);
+          }
+          find(element.childNodes);
+        }
+      });
+    }
+
+    find(this.childNodes);
+    return found;
+  }
+
+  function findStringSelector(selector) {
+    var list = getSelectorGroup(selector);
+    var found = [ [ this ] ];
+    var self = this;
+
+    function each (node) {
+      found.push(findPredicate.call(node, function (element) {
+        return element.is(list[0]);
+      }));
+    }
+
+    while (list.length) {
+      found[found.length - 1].forEach(each);
+      list.shift();
+    }
+
+    return found.slice(-1)[0];
+  }
+
+
+  Node.prototype.find = function (selector) {
+    if (typeof selector === 'string') {
+      return findStringSelector.call(this, selector);
+    } else if (typeof selector === 'function') {
+      return findPredicate.call(this, selector);
+    }
+    throw new Error('Invalid selector for \'find\'');
+  };
+}());
