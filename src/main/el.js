8c5c1371f8c3d1e8dc74e7ac5fce8664fa4a8692
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-24 21:40:56 -0500

    renamed 'opt' to 'props'

--- src/main/el.js
+++ src/main/el.js
@@ -1,8 +1,5 @@
 function el(tagName) {
-  var i = 1;
-  var n = arguments.length;
-
-  var opt = {};
+  var props = {};
   var children = [];
 
   switch (arguments.length) {
@@ -10,21 +7,23 @@ function el(tagName) {
       if (Array.isArray(arguments[1])) {
         children = arguments[1];
       } else {
-        opt = arguments[1];
+        props = arguments[1];
       }
       break;
     case 3 :
-      opt = arguments[1];
+      props = arguments[1];
       children = arguments[2];
       break;
   }
 
-  if (Component.lib[tagName]) {
-    return createComponent(tagName, opt, children);
+  if (Component.function[tagName]) {
+    return Component.function[tagName](props, children);
+  } else if (Component.lib[tagName]) {
+    return createComponent(tagName, props, children);
   } else if (typeof tagName === 'string') {
-    return new Node(tagName, opt, children);
+    return new Node(tagName, props, children);
   } else if (isElement(tagName)) {
-    return new Node(tagName, opt, children, true);
+    return new Node(tagName, props, children, true);
   } else {
     throw new Error('The first argument for "el" must be either a Component or a valid HTML tag name. eg: el("div")');
   }
