28e70bbd13f77e4a65bcc8a2dcea10038837abd7
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-25 08:29:21 -0500

    Rewrote 'hasParent'

--- src/main/Node/Node.prototype.hasParent.js
+++ src/main/Node/Node.prototype.hasParent.js
@@ -1,3 +1,13 @@
 Node.prototype.hasParent = function (target) {
-  return hasParent(this.node, target);
+  var node = this.node;
+
+  function contains(t) {
+    return (t.getNode ? t.getNode().node : t).contains(node);
+  }
+
+  if (Array.isArray(target)) {
+    return target.map(contains);
+  }
+
+  return contains(target);
 };
