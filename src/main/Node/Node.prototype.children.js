a102d842d3cf2402ab528285e032914d6453fbce
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-25 18:42:32 -0500

    Added ability to replace children when passed an array

--- src/main/Node/Node.prototype.children.js
+++ src/main/Node/Node.prototype.children.js
@@ -1,6 +1,14 @@
 Node.prototype.children = function (a, b) {
   if (typeof a === 'number' && typeof b === 'number') {
     return this.childNodes.slice(a, b);
+  } else if (Array.isArray(a)) {
+    this.childNodes = [];
+    this.node.innerHTML = '';
+    this.append(a);
+    return this;
   }
-  return typeof a === 'number' ? this.childNodes[a] : this.childNodes;
+
+  return typeof a === 'number'
+    ? this.childNodes[a]
+    : this.childNodes;
 };
