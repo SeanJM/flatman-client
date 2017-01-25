4a891e524465bd1d99a91fcf07c08c59d6a8ad80
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-24 21:39:36 -0500

    optimized

--- src/main/createComponent.js
+++ src/main/createComponent.js
@@ -1,57 +1,40 @@
-function createComponent(tagName, opt, array) {
-  var i = 1;
-  var n = arguments.length;
-  var k;
+function createComponent(tagName, props, array) {
   var constructor = Component.lib[tagName];
-  var hasAppend = typeof constructor.prototype.append === 'function';
-  var hasText = typeof constructor.prototype.text === 'function';
-  var component = new constructor(opt);
+  var component = new constructor(props);
   var children = [];
   var strings = [];
-
-  function getNames(node) {
-    if (node.children) {
-      node.children().forEach(function (child) {
-        var name = child.name();
-        if (name) {
-          component.node[name] = child;
-        }
-        getNames(child);
-      });
-    }
-  }
 
   component.tagName = tagName;
   component.node = component.node || {};
   component.dict = component.dict || {};
 
-  if (hasText) {
-    array.forEach(function (child) {
-      if (typeof child === 'string' || typeof child === 'number') {
-        strings.push(child);
+  if (constructor.prototype.text) {
+    for (var i = 0, n = array.length; i < n; i++) {
+      if (typeof array[i] === 'string' || typeof array[i] === 'number') {
+        strings.push(array[i]);
       } else {
-        children.push(child);
+        children.push(array[i]);
       }
-    });
+    }
   } else {
     children = array;
   }
 
-  for (k in opt) {
-    component.dict[k] = opt[k];
+  for (var prop in props) {
+    component.dict[prop] = props[prop];
   }
 
   if (typeof component.render === 'function') {
-    component.node.document = component.render(opt);
+    component.node.document = component.render(props);
     if (component.node.document) {
-      component.node.document.componentTagName = tagName;
-      getNames(component.node.document);
+      component.node.document.component = component;
+      getComponentNames(component, component.node.document);
     } else {
       throw new Error('Invalid component, component must return a node in the render function.');
     }
   }
 
-  if (children.length && hasAppend) {
+  if (children.length) {
     component.append(children);
   }
 
