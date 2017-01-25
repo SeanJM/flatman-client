232c77b1baf59dab0f978b2ee044f589ab15a10b
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-24 21:39:07 -0500

    moved out of 'createComponent'

---
+++ src/common/getComponentNames.js
@@ -0,0 +1,11 @@
+function getComponentNames(component, node) {
+  if (node.children) {
+    node.children().forEach(function (child) {
+      var name = child.name && child.name();
+      if (name) {
+        component.node[name] = child.component || child;
+      }
+      getComponentNames(component, child);
+    });
+  }
+}
