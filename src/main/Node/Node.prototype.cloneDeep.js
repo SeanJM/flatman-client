91267a2e1ba01b1714d2500b0c719bad4fcf4386
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-24 21:40:08 -0500

    simplified

--- src/main/Node/Node.prototype.cloneDeep.js
+++ src/main/Node/Node.prototype.cloneDeep.js
@@ -1,18 +1,3 @@
 Node.prototype.cloneDeep = function () {
-  var elNode = el(this.node.cloneNode(true));
-
-  function getRel(elNode) {
-    [].forEach.call(elNode.node.childNodes, function (node) {
-      var childElNode;
-      if (isElement(node)) {
-        childElNode = el(node);
-        elNode.childNodes.push(childElNode);
-        childElNode.parentNode = elNode;
-        getRel(childElNode);
-      }
-    });
-  }
-
-  getRel(elNode);
-  return elNode;
+  return el(this.node.cloneNode(true)).mapChildren();
 };
