2d52ca80bec8048fdbfad15011334595ea54c7e4
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-31 07:38:14 -0500

    changed target to 'document.body'

--- src/init/initDragAndDrop.js
+++ src/init/initDragAndDrop.js
@@ -1,5 +1,5 @@
 function initDragAndDrop() {
-  document.addEventListener('mousedown', function (e) {
+  document.body.addEventListener('mousedown', function (e) {
     var startX = e.pageX;
     var startY = e.pageY;
     var isDrag = false;
@@ -20,13 +20,13 @@ function initDragAndDrop() {
         cancelable : true
       });
 
-      document.removeEventListener('mousemove', dragmove);
-      document.removeEventListener('mouseup', dragend);
+      document.body.removeEventListener('mousemove', dragmove);
+      document.body.removeEventListener('mouseup', dragend);
 
       if (e.which === 1 && isDrag) {
         isDrag = false;
-        document.style[ VENDOR_PREFIX.userSelect ] = '';
-        document.style.cursor = '';
+        document.body.style[ VENDOR_PREFIX.userSelect ] = '';
+        document.body.style.cursor = '';
         target.dispatchEvent(eve);
       }
     }
@@ -59,8 +59,8 @@ function initDragAndDrop() {
 
     // Ensure it's left click which starts the dragging
     if (e.which === 1) {
-      document.addEventListener('mouseup', dragend);
-      document.addEventListener('mousemove', dragmove);
+      document.body.addEventListener('mouseup', dragend);
+      document.body.addEventListener('mousemove', dragmove);
     }
   });
 }
