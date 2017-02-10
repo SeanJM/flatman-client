7430412f10724da67cfb33a93ec74ed8872e3998
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-02-10 12:05:50 -0500

    Added support for single node

--- src/main/Node/Node.prototype.append.js
+++ src/main/Node/Node.prototype.append.js
@@ -1,10 +1,10 @@
 Node.prototype.append = function (array) {
   if (arguments.length > 1) {
-    throw 'You have too many arguments (' + arguments.length + ') for \'.append\', it takes a single array.';
+    throw 'You have too many arguments (' + arguments.length + ') for \'.append\', it takes a node or an array of nodes.';
   } else if (Array.isArray(array)) {
     appendChild(this, array);
   } else {
-    throw 'Invalid argument type (' + typeof array + ') for .append';
+    appendChild(this, [ array ]);
   }
   return this;
 };
---
+++ test/selenium-tests/append_single_node.js
@@ -0,0 +1,10 @@
+var el = flatman.el;
+var a = el('div');
+var b = el('div');
+
+a.append(b);
+
+return {
+  left : a.children()[0].node === b.node,
+  right : true
+};
