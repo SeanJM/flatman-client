1fd11b4cf26924d530f2aa65cb4fdd4c5819f0e6
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-02-17 11:28:14 -0500

    Added 'after' fixed bug in 'before'

--- src/main/Node/Node.prototype.before.js
+++ src/main/Node/Node.prototype.before.js
@@ -1,16 +1,17 @@
 Node.prototype.before = function (children) {
   var f = document.createDocumentFragment();
+  var childNodes = this.parentNode.childNodes;
 
   if (Array.isArray(children)) {
     for (var i = 0, n = children.length; i < n; i++) {
-      f.append(getNode(children[i]));
+      f.appendChild(getNode(children[i]));
     }
   } else if (!Array.isArray(children) || arguments.length > 1) {
     throw 'Before takes a single array as an argument';
   }
 
   this.node.parentNode.insertBefore(f, this.node);
-  this.parentNode.childNodes = this.parentNode.childNodes.concat(children);
+  [].splice.apply(childNodes, [ childNodes.indexOf(this), 0].concat(children));
   [].forEach.call(f, mount);
 
   return this;
