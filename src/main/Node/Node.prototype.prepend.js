318ab0aec5bb0ef63660f1f443d8598e7b075055
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-02-17 20:47:23 -0500

    Simplified 'prepend' method

--- src/main/Node/Node.prototype.prepend.js
+++ src/main/Node/Node.prototype.prepend.js
@@ -1,33 +1,3 @@
 Node.prototype.prepend = function (children) {
-  var i = 0;
-  var n = children.length;
-  var f = document.createDocumentFragment();
-  var childrenByNode = [];
-
-  if (!this.node.parentNode) {
-    throw 'Cannot perform \'prepend\', node requires a parent';
-  }
-
-  if (Array.isArray(children)) {
-    for (; i < n; i++) {
-      if (children[i]) {
-        childrenByNode[i] = children[i].getNode
-          ? children[i].getNode()
-          : children[i];
-
-        if (childrenByNode[i].node) {
-          childrenByNode[i].parentNode = this;
-          f.appendChild(childrenByNode[i].node);
-        } else {
-          childrenByNode[i] = new Text(childrenByNode[i]);
-          f.appendChild(new Text(childrenByNode[i]));
-        }
-      }
-    }
-
-    this.node.parentNode.insertBefore(f, this.node);
-    [].unshift.apply(this.childNodes, childrenByNode.reverse());
-  } else {
-    this.prepend([ children ]);
-  }
+  this.children()[0].before(children)
 };
