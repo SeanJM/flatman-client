67d282d788dfff37dda2f9b4682e7d5ce645fb80
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-02-25 16:52:27 -0500

    names will no longer be overriden

--- src/common/getComponentNames.js
+++ src/common/getComponentNames.js
@@ -2,7 +2,7 @@ function getComponentNames(component, node) {
   if (node.children) {
     node.children().forEach(function (child) {
       var name = child.name && child.name();
-      if (name) {
+      if (name && !component.node[name]) {
         component.node[name] = child.component || child;
       }
       getComponentNames(component, child);
