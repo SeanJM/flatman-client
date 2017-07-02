a9542b9fa445bcf7199d643e6b3cd33957b86bc8
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-07-02 08:41:23 -0400

    Added while loop to return the highest level component

--- src/common/getComponentNames.js
+++ src/common/getComponentNames.js
@@ -2,8 +2,12 @@ function getComponentNames(component, node) {
   if (node.children) {
     node.children().forEach(function (child) {
       var name = child.name && child.name();
+      var temp = child;
       if (name && !component.node[name]) {
-        component.node[name] = child.component || child;
+        while (temp.component) {
+          temp = temp.component;
+        }
+        component.node[name] = temp;
       }
       getComponentNames(component, child);
     });
